ba1adfa75a94d5d54099ce7d4a3cf0e1480c360a
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-20 11:17:44 +0900

    enumの設定

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,13 @@ class Movie < ApplicationRecord
     validates :title
     validates :url
   end
+
+  enum genre: {
+    invisible: 0,
+    basic: 1,
+    git: 2,
+    ruby: 3,
+    rails: 4,
+    php: 5
+  }
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -4,4 +4,13 @@ class Text < ApplicationRecord
     validates :title
     validates :content
   end
+
+  enum genre: {
+    invisible: 0,
+    basic: 1,
+    git: 2,
+    ruby: 3,
+    rails: 4,
+    php: 5
+  }
 end
