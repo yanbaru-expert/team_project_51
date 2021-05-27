77876b2bdd17ced7933c3ad8951f8f0eccca3832
ryo <if14germany1248@icloud.com>
2021-05-27 21:54:21 +0900

    コメントアウトを解除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
   def max_width
     if controller_name == "texts" && action_name == "show"
       "mw-md"
-      # Devise 導入後にコメントアウトを解除
-      # elsif devise_controller?
-      #  "mw-sm"
+    elsif devise_controller?
+      "mw-sm"
     else
       "mw-xl"
     end
