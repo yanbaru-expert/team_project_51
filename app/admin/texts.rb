b5019a9f04591ec8a7571767e1f7d8eca34d9f2a
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-29 16:43:19 +0900

    管理画面の各ページのカラム・フォームを再修正

--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -1,4 +1,43 @@
 ActiveAdmin.register Text do
+  permit_params :genre, :title, :content
+
+  index do
+    selectable_column
+    id_column
+    column :genre, :text, &:genre_i18n
+    column :title
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :genre, :text, &:genre_i18n
+      row :title
+      row :content
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :genre, as: :select, collection: Text.genres_i18n.invert
+      input :title
+      input :content
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Text.genres_i18n.invert.transform_values { |v| Text.genres[v] }
+  filter :title
+  filter :content
+  filter :created_at
+  filter :updated_at
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,44 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :password
+    column :password_confirmation
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :email
+      row :password
+      row :password_confirmation
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :email
+      input :password
+      input :password_confirmation
+    end
+    f.actions
+  end
+
+  filter :email
+  filter :password
+  filter :password_confirmation
+  filter :created_at
+  filter :updated_at
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
