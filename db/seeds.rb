e40a6d42fec7cc305d7f02191553f7bb2154e241
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-29 10:35:16 +0900

    Active Admin の導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
