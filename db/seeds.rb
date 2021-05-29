bf70c2ecfd065013ac7b0afb682a21e3ddc3681f
stan <>
2021-05-30 00:21:19 +0900

    修正しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
+require "csv"
+
 # texts, movies テーブルを再生成（関連付くテーブルを含む）
 %w[texts movies].each do |table_name|
   ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
 end
-
-require "csv"
 
 CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
   Text.create!(
