4e92553fecade3367a1f3eb16adffec0d63e644a
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-29 10:45:50 +0900

    AdminUser.create! の1行を User と同様の形式に修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 email = "test@example.com"
 password = "password"
+admin_email = "admin@example.com"
 
 User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+AdminUser.find_or_create_by!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
