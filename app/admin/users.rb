e81f2bb4196021a6f015b6c4386374108f4fe182
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-29 11:48:39 +0900

    管理画面の各ページのカラム・フォームを修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,6 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
+  config.skip_before_action :authenticate_user!
   config.site_title = "Team Project"
 
   # Set the link url for the title. For example, to take
