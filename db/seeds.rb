ea911f901b0a2fd3f5900e0f88bd737f73012f76
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-30 10:56:24 +0900

    db/seeds.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
-AdminUser.find_or_create_by!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+AdminUser.find_or_create_by!(email: admin_email, password: password, password_confirmation: password) if Rails.env.development?
