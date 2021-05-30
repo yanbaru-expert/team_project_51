4fb1700c1f18e2416dcf40190b95460e5d2c8590
kagimotoeiji <thank.eiji1017@gmail.com>
2021-05-30 16:49:01 +0900

    db/seeds.rb再々修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,5 @@ end
 
 AdminUser.find_or_create_by!(email: admin_email) do |adminuser|
   adminuser.password = password
-  adminuser.password_confirmation = password
   puts "管理者ユーザーの初期データインポートに成功しました。"
 end
