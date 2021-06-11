ca7dd809e54efaf1e77f06e3f20ace203094d3bc
stan <>
2021-06-11 23:30:14 +0900

    修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.includes(:watch_progresses).references(:all)
+    @movies = Movie.includes(:watch_progresses).where(genre: Movie::RAILS_GENRE_LIST)
   end
 
   def show; end
