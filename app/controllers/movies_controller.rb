e18ac3d357a82eb785474993952a7803a6d37f4f
ryo <if14germany1248@icloud.com>
2021-06-11 15:50:19 +0900

    add pagination

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
+  PER_PAGE = 12
+
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    @movies = Movie.page(params[:page]).per(PER_PAGE)
   end
 
   def show; end
