536bb224d20aec7ae17dfb6bd5e43b7482c1eb1e
ryo <if14germany1248@icloud.com>
2021-06-13 15:20:50 +0900

    修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   PER_PAGE = 12
 
   def index
-    @movies = Movie.page(params[:page]).per(PER_PAGE)
+    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
   end
 
   def show; end
