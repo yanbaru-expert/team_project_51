ce89b06d98a9630781c5dd4c8db53977158857dc
kohei <tk1124e25@gmail.com>
2021-06-25 16:31:25 +0900

    php動画の一覧画面作成(タイトル未変更)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,11 @@ class MoviesController < ApplicationController
   PER_PAGE = 12
 
   def index
-    @movies = Movie.includes(:watch_progresses).where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+    @movies = if params[:genre] == "php"
+                Movie.includes(:watch_progresses).where(genre: Movie::PHP_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+              else
+                Movie.includes(:watch_progresses).where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+              end
   end
 
   def show; end
